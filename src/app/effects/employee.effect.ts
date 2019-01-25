c0acbc1271a9e3382095623bac5adef09fc3a028
rappad <wasuwat.se@gmail.com>
2019-01-25 17:22:21 +0700

    add effects typing

--- src/app/effects/employee.effect.ts
+++ src/app/effects/employee.effect.ts
@@ -4,13 +4,13 @@ import { Actions, Effect, ofType } from "@ngrx/effects"
 import { of, Observable } from "rxjs"
 import { pluck, map, catchError, switchMap } from "rxjs/operators"
 import { EmployeeService } from "../services/employee.service"
-import { ActionTypes } from "../actions/employee.action"
+import { ActionTypes, EmployeeActions } from "../actions/employee.action"
 
 @Injectable()
 export class EmployeeEffects {
   @Effect()
   loadEmployee$: Observable<Action> = this.actions$.pipe(
-    ofType(ActionTypes.LOAD_REQUEST),
+    ofType<EmployeeActions>(ActionTypes.LOAD_REQUEST),
     switchMap(() =>
       this.employeeService.get().pipe(
         pluck("data"),
